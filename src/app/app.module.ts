c09dc7a59b82c85d9161a1988a9892360dd19fa8
victor fonseca <victorfonsecass@outlook.com>
2024-07-09 19:07:59 -0300

    results component criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FlightSearchComponent } from './core/flight-search/flight-search.component';
 import { HomeComponent } from './core/home/home.component';
+import { ResultsComponent } from './core/results/results.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FlightSearchComponent,
-    HomeComponent
+    HomeComponent,
+    ResultsComponent
   ],
   imports: [
     BrowserModule,
